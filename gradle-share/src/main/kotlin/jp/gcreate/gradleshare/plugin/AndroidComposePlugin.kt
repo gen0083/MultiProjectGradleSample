e703cd081b88d9cbd04951fce8e8ada5a97ff79f
Gen <archiherewego@gmail.com>
2024-01-22 16:27:07 +0900

    Delete unused library notation

--- gradle-share/src/main/kotlin/jp/gcreate/gradleshare/plugin/AndroidComposePlugin.kt
+++ gradle-share/src/main/kotlin/jp/gcreate/gradleshare/plugin/AndroidComposePlugin.kt
@@ -43,7 +43,6 @@ class AndroidComposePlugin : Plugin<Project> {
                 implementation(libs.library("composeUiToolingPreview"))
                 implementation(libs.library("androidxLifecycleLifecycleRuntimeKtx"))
                 implementation(libs.library("androidxActivityActivityCompose"))
-                implementation(libs.library("lottieCompose"))
                 testImplementation(libs.library("junit"))
                 testImplementation(libs.library("androidxTestExtJunit"))
                 testImplementation(libs.library("androidxTestEspressoEspressoCore"))
