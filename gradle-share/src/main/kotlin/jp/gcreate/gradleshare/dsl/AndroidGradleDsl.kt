86f03df7394365ffb456f227a7daf0cfe8adf0da
Gen <archiherewego@gmail.com>
2024-01-22 15:21:53 +0900

    buildComposeMetricsを共有するため外出し

--- gradle-share/src/main/kotlin/jp/gcreate/gradleshare/dsl/AndroidGradleDsl.kt
+++ gradle-share/src/main/kotlin/jp/gcreate/gradleshare/dsl/AndroidGradleDsl.kt
@@ -62,3 +62,29 @@ fun Project.setupAndroid() {
         }
     }
 }
+
+fun Project.buildComposeMetricsParameters(): List<String> {
+    val metricParameters = mutableListOf<String>()
+    val enableMetricsProvider = project.providers.gradleProperty("enableComposeCompilerMetrics")
+    val relativePath = projectDir.relativeTo(rootDir)
+
+    val enableMetrics = (enableMetricsProvider.orNull == "true")
+    if (enableMetrics) {
+        val metricsFolder = rootProject.buildDir.resolve("compose-metrics").resolve(relativePath)
+        metricParameters.add("-P")
+        metricParameters.add(
+            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath
+        )
+    }
+
+    val enableReportsProvider = project.providers.gradleProperty("enableComposeCompilerReports")
+    val enableReports = (enableReportsProvider.orNull == "true")
+    if (enableReports) {
+        val reportsFolder = rootProject.buildDir.resolve("compose-reports").resolve(relativePath)
+        metricParameters.add("-P")
+        metricParameters.add(
+            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
+        )
+    }
+    return metricParameters.toList()
+}
--- gradle-share/src/main/kotlin/jp/gcreate/gradleshare/plugin/AndroidComposePlugin.kt
+++ gradle-share/src/main/kotlin/jp/gcreate/gradleshare/plugin/AndroidComposePlugin.kt
@@ -1,6 +1,7 @@
 package jp.gcreate.gradleshare.plugin
 
 import jp.gcreate.gradleshare.dsl.android
+import jp.gcreate.gradleshare.dsl.buildComposeMetricsParameters
 import jp.gcreate.gradleshare.dsl.debugImplementation
 import jp.gcreate.gradleshare.dsl.implementation
 import jp.gcreate.gradleshare.dsl.library
@@ -55,28 +56,3 @@ class AndroidComposePlugin : Plugin<Project> {
     }
 }
 
-private fun Project.buildComposeMetricsParameters(): List<String> {
-    val metricParameters = mutableListOf<String>()
-    val enableMetricsProvider = project.providers.gradleProperty("enableComposeCompilerMetrics")
-    val relativePath = projectDir.relativeTo(rootDir)
-
-    val enableMetrics = (enableMetricsProvider.orNull == "true")
-    if (enableMetrics) {
-        val metricsFolder = rootProject.buildDir.resolve("compose-metrics").resolve(relativePath)
-        metricParameters.add("-P")
-        metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath
-        )
-    }
-
-    val enableReportsProvider = project.providers.gradleProperty("enableComposeCompilerReports")
-    val enableReports = (enableReportsProvider.orNull == "true")
-    if (enableReports) {
-        val reportsFolder = rootProject.buildDir.resolve("compose-reports").resolve(relativePath)
-        metricParameters.add("-P")
-        metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
-        )
-    }
-    return metricParameters.toList()
-}
