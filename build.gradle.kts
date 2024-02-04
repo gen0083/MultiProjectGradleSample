c8d08be0f83d9110564a318bd5840055254ff0ee
Gen <archiherewego@gmail.com>
2024-02-04 21:58:20 +0900

    不要な宣言を削除
    apply falseでの読み込みはpre-compiled scriptで依存関係として読み込んでいるため不要
    書いてても悪さするものではないけど

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidGradlePlugin) apply false
-    alias(libs.plugins.androidGradleLibraryPlugin) apply false
-    alias(libs.plugins.kotlinGradlePlugin) apply false
     id("jp.gcreate.versionCatalogUpdate")
     id("jp.gcreate.dependencyAnalysis")
 }
