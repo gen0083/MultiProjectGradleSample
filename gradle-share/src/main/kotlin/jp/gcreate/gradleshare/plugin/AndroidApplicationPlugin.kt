4d46bc8b3bc9804397d28ac599e9236f8fdab8b7
Gen <archiherewego@gmail.com>
2024-02-06 00:26:38 +0900

    setupAndroidをやめて直接展開
    DSLの方に定義があるのは違和感があるのでPluginで定義するようにした
    ついでにcompileSdkに置き換え
    またlibraryではtargetSdkが削除予定だった

--- gradle-share/src/main/kotlin/jp/gcreate/gradleshare/plugin/AndroidApplicationPlugin.kt
+++ gradle-share/src/main/kotlin/jp/gcreate/gradleshare/plugin/AndroidApplicationPlugin.kt
@@ -1,9 +1,13 @@
 package jp.gcreate.gradleshare.plugin
 
 import jp.gcreate.gradleshare.dsl.android
-import jp.gcreate.gradleshare.dsl.setupAndroid
+import jp.gcreate.gradleshare.dsl.library
+import jp.gcreate.gradleshare.dsl.libs
+import jp.gcreate.gradleshare.dsl.version
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidApplicationPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -13,7 +17,29 @@ class AndroidApplicationPlugin : Plugin<Project> {
             }
 
             android {
-                setupAndroid()
+                namespace?.let {
+                    this.namespace = it
+                }
+                compileSdk = libs.version("compileSdk").toInt()
+
+                defaultConfig {
+                    minSdk = libs.version("minSdk").toInt()
+                    targetSdk = libs.version("targetSdk").toInt()
+                }
+
+                compileOptions {
+                    sourceCompatibility = JavaVersion.toVersion(libs.version("jvmVersion").toInt())
+                    targetCompatibility = JavaVersion.toVersion(libs.version("jvmVersion").toInt())
+                    isCoreLibraryDesugaringEnabled = true
+                }
+                dependencies {
+                    add("coreLibraryDesugaring", libs.library("androidDesugarJdkLibs"))
+                }
+                testOptions {
+                    unitTests {
+                        isIncludeAndroidResources = true
+                    }
+                }
             }
         }
     }
--- gradle-share/src/main/kotlin/jp/gcreate/gradleshare/plugin/AndroidLibraryPlugin.kt
+++ gradle-share/src/main/kotlin/jp/gcreate/gradleshare/plugin/AndroidLibraryPlugin.kt
@@ -1,9 +1,13 @@
 package jp.gcreate.gradleshare.plugin
 
 import jp.gcreate.gradleshare.dsl.androidLibrary
-import jp.gcreate.gradleshare.dsl.setupAndroid
+import jp.gcreate.gradleshare.dsl.library
+import jp.gcreate.gradleshare.dsl.libs
+import jp.gcreate.gradleshare.dsl.version
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 @Suppress("unused")
 class AndroidLibraryPlugin : Plugin<Project> {
@@ -14,7 +18,30 @@ class AndroidLibraryPlugin : Plugin<Project> {
             }
 
             androidLibrary {
-                setupAndroid()
+                namespace?.let {
+                    this.namespace = it
+                }
+                compileSdk = libs.version("compileSdk").toInt()
+
+                defaultConfig {
+                    minSdk = libs.version("minSdk").toInt()
+                    // libraryではtargetSdkは削除される予定（gradle9.0から）→たぶん依存側のtargetSdkが使われるんだと思う
+                    targetSdk = libs.version("targetSdk").toInt()
+                }
+
+                compileOptions {
+                    sourceCompatibility = JavaVersion.toVersion(libs.version("jvmVersion").toInt())
+                    targetCompatibility = JavaVersion.toVersion(libs.version("jvmVersion").toInt())
+                    isCoreLibraryDesugaringEnabled = true
+                }
+                dependencies {
+                    add("coreLibraryDesugaring", libs.library("androidDesugarJdkLibs"))
+                }
+                testOptions {
+                    unitTests {
+                        isIncludeAndroidResources = true
+                    }
+                }
             }
         }
     }
